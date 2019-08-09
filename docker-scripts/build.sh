e25f1784a0aa4f25a73c6771eebc91745cfbdb61
William Bernting <wbern@users.noreply.github.com>
2019-08-09 22:06:48 +0200

    add a timestamp

--- docker-scripts/build.sh
+++ docker-scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 node ./common/scripts/install-run-rush.js update --bypass-policy
-
+echo "// Hello world at $(date +"%T")!" >> ./libraries/my-controls/src/index.ts
 node ./common/scripts/install-run-rush.js build
